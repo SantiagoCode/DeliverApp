a43d4e4663dc64fd0ff5f2f673b76bc5d47e69bb
Santiago Salazar <santiagosalazar.dev@gmail.com>
2022-04-18 01:31:18 -0400

    Primer commit. Estoy haciendo la interfaz

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const path = require("path");
+
+// settings
+app.set('port', 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// routes
+app.use(require('./routes/index'));
+
+// static files
+app.use(express.static(path.join(__dirname, "/public")));
+
+// listening the server
+app.listen(app.get('port'), () => {
+  console.log(`Servidor funcionando en el puerto ${app.get('port')}`);
+});
+
